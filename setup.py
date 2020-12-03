67c7d614b560d92a67349c2139dd98c90542c07f
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:06:51 +0300

    2020:12:04 00:06

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-finder',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/.DS_Store','scripts/.finder-alias.applescript','scripts/.finder-close-bg.applescript','scripts/.finder-close-duplicates.applescript','scripts/.finder-comment.applescript','scripts/.finder-icon.applescript','scripts/.finder-reveal.applescript','scripts/.finder-selection.applescript','scripts/finder','scripts/finder-alias','scripts/finder-close-bg','scripts/finder-close-duplicates','scripts/finder-comment','scripts/finder-exec','scripts/finder-frontmost','scripts/finder-icon','scripts/finder-reveal','scripts/finder-selection']
